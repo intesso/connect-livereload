6a92cbfca16f53c8a070c7ecd2550c5902e44c23
andi-neck <andi.neck@intesso.com>
2015-06-29 08:38:55 +0200

    closes PR #54, #58 #59

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function livereload(opt) {
   // options
-  var opt = opt || {};
+  opt = opt || {};
   var ignore = opt.ignore || opt.excludeList || [/\.js(\?.*)?$/, /\.css(\?.*)?$/, /\.svg(\?.*)?$/, /\.ico(\?.*)?$/,
     /\.woff(\?.*)?$/, /\.png(\?.*)?$/, /\.jpg(\?.*)?$/, /\.jpeg(\?.*)?$/, /\.gif(\?.*)?$/, /\.pdf(\?.*)?$/,
     /\.json(\?.*)?$/
@@ -22,7 +22,7 @@ module.exports = function livereload(opt) {
   var port = opt.port || 35729;
 
   function snippet(host) {
-    var src = '//' + host + ':' + port + '/livereload.js?snipver=1';
+    var src = opt.src || '//' + host + ':' + port + '/livereload.js?snipver=1';
     return '<script src="' + src + '" async="" defer=""></script>';
   }
 
